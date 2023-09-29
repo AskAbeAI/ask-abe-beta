22d83a399bfe530fbb28467183b2c58ff51c0be6
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-09-29 13:31:39 -0700

    blah

--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -13,64 +13,68 @@ import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { ApplicationError, UserError } from '@/lib/errors'
 import { createClient } from "@supabase/supabase-js"
 import { runAbe } from "./askAbe"
-import { Readable, Stream } from 'stream'
+import { Readable } from 'stream';  // Make sure you have this at the top if using Readable streams
+// ... other necessary imports ...
 
+export default async function handler(req: NextRequest, res: NextApiResponse) {
+  console.log("In handler function...");
 
-
-export default async function handler(req: NextRequest, res: NextApiResponse): Promise<string> => {
-  console.log("In handler function...")
   if (req.method !== 'POST') {
     res.setHeader('Allow', 'POST');
-    return res.status(405).end('Method Not Allowed'); // Only allow POST requests
+    return res.status(405).end('Method Not Allowed');
   }
-  const requestData: { question:string, dataset:Dataset, apiKey:string } =
-      (await req.json()) as AnswerBody;
-  
-    const stream = new Readable({
-        read() {}
-    });
+
+  const requestData: { question: string, dataset: Dataset, apiKey: string } = await req.json();
+
+  const stream = new Readable({
+    read() {}
+  });
+
   const config = new Configuration({
     apiKey: requestData.apiKey,
-  })
-  console.log(requestData.dataset)
-  console.log(requestData.question)
-  console.log(requestData.apiKey)
+  });
+
+  console.log(requestData.dataset);
+  console.log(requestData.question);
+  console.log(requestData.apiKey);
+
   let citations = "";
   try {
     if (!requestData.apiKey) {
-      throw new ApplicationError('Missing environment variable OPENAI_KEY')
+      throw new ApplicationError('Missing environment variable OPENAI_KEY');
     }
-    const openai = new OpenAIApi(config)
+
+    const openai = new OpenAIApi(config);
 
     if (!requestData) {
-      throw new UserError('Missing request data')
+      throw new UserError('Missing request data');
     }
 
     if (!requestData.question) {
-      throw new UserError('Missing query in request data')
+      throw new UserError('Missing query in request data');
     }
 
-    console.log("Sanitizing user query...")
-    // Moderate the content to comply with OpenAI T&C
-    const sanitizedQuery = requestData.question.trim()
+    console.log("Sanitizing user query...");
+
+    const sanitizedQuery = requestData.question.trim();
     const moderationResponse: CreateModerationResponse = await openai
       .createModeration({ input: sanitizedQuery })
-      .then((res) => res.json())
+      .then((res) => res.json());
 
-    const [results] = moderationResponse.results
-    console.log("Checking if results are flagged...")
+    const [results] = moderationResponse.results;
+    console.log("Checking if results are flagged...");
     if (results.flagged) {
       throw new UserError('Flagged content', {
         flagged: true,
         categories: results.categories,
-      })
+      });
     }
 
     res.setHeader('Content-Type', 'text/plain');
     res.status(200);
-    // Transform the response into a readable stream
-    console.log(sanitizedQuery)
-    const client = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+
+    console.log(sanitizedQuery);
+    const client = createClient('https://xyzcompany.supabase.co', 'public-anon-key');
 
     for await (const text of runAbe(client, sanitizedQuery, requestData.apiKey)) {
       if (text.includes("[DONE]")) {
@@ -80,16 +84,8 @@ export default async function handler(req: NextRequest, res: NextApiResponse): P
     }
   } catch (error) {
     console.error("An error occurred:", error);
-    return "Error";
   } finally {
-    stream.push(null);  // Signal the end of data
-    // Set the headers (e.g., for plain text data)
-    // Pipe the stream to the response
+    stream.push(null);
     stream.pipe(res);
-    return citations;
   }
-};
-
-
-
-
+}
