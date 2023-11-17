94797ac64ff4639d963f85ac3882915f01c50cd2
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-11-16 18:57:39 -0800

    obliterated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,49 +1,9 @@
 "use client";
 import React, { useState } from 'react';
-import Image from 'next/image';
-import ChatOptionToggle from '@/components/ui/chatOptionToggle';
-import { hourglass } from 'ldrs';
-import { bouncy } from 'ldrs'
-
-
-
-
-import { newtonsCradle } from 'ldrs'
-import OptionsList from '@/components/optionsFilter';
-
 
 
 export default function aboutPage() {
   
-
-
-
-  // Default values shown
-  // <div className="grid-cols-5 p-10">
-
-
-  //   <l-newtons-cradle
-  //     size="78"
-  //     speed="1.4"
-  //     color="black"
-  //   />
-
-
-
-  //   <l-hourglass
-  //     size="40"
-  //     bg-opacity="0.1"
-  //     speed="1.75"
-  //     color="black"
-  //   />
-  //   <l-bouncy
-  //     size="45"
-  //     speed="1.75"
-  //     color="black"
-  //   />
-  // </div>
-
-
   return (
     <div></div>
   );
