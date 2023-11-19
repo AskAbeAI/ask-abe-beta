97ebfd9e0921e9703e339253f459c605d0fd3c6b
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-11-19 14:00:09 -0800

    removing ignored routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/about", "/legal", "/how", "/devlog", "/devlogblog", "/playground", "/api/(.*)"], 
-  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)"],
+  publicRoutes: ["/", "/about", "/legal", "/how", "/devlog", "/devlogblog", "/playground", "/api/(.*)"]
 });
 
 export const config = {
