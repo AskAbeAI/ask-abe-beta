508836c749f5d93ded3587552c2969b832170309
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-11-19 13:13:19 -0800

    debugging middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,11 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/about", "/legal", "/how", "/devlog", "/playground", "/(api|trpc)(.*)"],
+  publicRoutes: ["/", "/about", "/legal", "/how", "/devlog", "/playground", "/api/*"],
 });
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
 
 
