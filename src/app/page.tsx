ac611e2ad4d61426fe60e760557f7eebbb7c5075
Madeline <datbihmad@gmail.com>
2023-11-22 10:41:40 -0500

    our team

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function HomePage() {
       <LandingSection />
       <LegalResearchComponent />
       <FeaturesSection/>
-     {/* <OurTeam/> */}
+     <OurTeam/>
 
 
       <PageFooter />
