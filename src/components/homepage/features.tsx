5bd88a0b6a1e04566130791d7c755d8c9103bed2
Madeline <datbihmad@gmail.com>
2023-11-22 10:15:44 -0500

    fixes

--- src/components/homepage/features.tsx
+++ src/components/homepage/features.tsx
@@ -10,7 +10,7 @@ const FeaturesSection: React.FC = () => {
     </h1>
     <div className="self-stretch mt-20 mb-24 pl-8 pr-8 max-md:max-w-full max-md:my-10 max-md:px-5">
       <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <div className="flex flex-col items-stretch w-[22%] max-md:w-full max-md:ml-0">
+        <div className="flex flex-col items-stretch w-[20%] max-md:w-full max-md:ml-0">
           <div className="items-stretch flex flex-col mt-2.5 max-md:mt-10">
             <h2 className="text-black text-center font-imfell text-2xl font-bold leading-9">
             Expandable Citations
@@ -27,14 +27,14 @@ const FeaturesSection: React.FC = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-col items-stretch w-[56%] ml-5 max-md:w-full max-md:ml-0">
+        <div className="flex flex-col justify-center items-stretch w-[60%] ml-5 max-md:w-full max-md:ml-0">
           <img
             loading="lazy"
-            srcSet="/features.png"
+            srcSet="/feature.png"
             alt="Feature Image"
           />
         </div>
-        <div className="flex flex-col items-stretch w-[22%] ml-5 max-md:w-full max-md:ml-0">
+        <div className="flex flex-col items-stretch w-[20%] ml-5 max-md:w-full max-md:ml-0">
           <div className="items-stretch self-stretch flex grow flex-col max-md:mt-10">
             <h2 className="text-black text-center font-imfell text-2xl font-bold leading-9">
             Chat Options & Jurisdictions
