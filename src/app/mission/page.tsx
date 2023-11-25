67e793beec72a0195a0bfea977f759eac2b92cfe
Madeline <datbihmad@gmail.com>
2023-11-25 10:30:34 -0500

    more mission

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -10,37 +10,30 @@ export default function MissionPage() {
       <div className="flex-col items-center overflow-hidden relative flex min-h-[382px] px-20 max-md:px-5">
         <img loading="lazy" src="/empowering.jpg" className="absolute inset-0 w-full h-full object-cover" alt="Empowering Image" />
         
-        <div className="relative text-[#FEFDF9] text-center text-5xl font-cinzel font-bold leading-[80px] max-w-screen-md mt-28 max-md:max-w-full max-md:mt-10">
-          OUR MISSION
+        <div className="relative top-1/2 text-[#FEFDF9] text-center text-5xl font-raleway font-bold leading-[80px] max-w-screen-md mt-28 max-md:max-w-full max-md:mt-10">
+          OUR VISION
         </div>
         
       </div>
       <div className="bg-[#FAF5E6] flex w-full flex-col  px-16 py-6 ">
       <div className="text-[#2F3F3D] text-7xl font-cinzel leading-[58px] mt-16 max-md:max-w-full max-md:text-4xl max-md:leading-[54px] max-md:mt-10">
         Informed Empowerment <br />
-        <div className="text-5xl pt-5">
-           Making Law Understandable for Everyone.
+        <div className="text-5xl pt-5 font-raleway ">
+           Guiding regular citizens to better understand primary source legislation
            </div>
       </div>
       <div className="text-black text-base font-fauna leading-6 mt-6 max-md:max-w-full">
-        Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus id
-        scelerisque est ultricies ultricies. Duis est sit sed leo nisl, blandit
-        elit sagittis. Quisque tristique consequat quam sed. Nisl at scelerisque
-        amet nulla purus habitasse.
+        At Ask Abe, our goal is to offer free legal education to everyone. 
       </div>
       <div className="text-black text-base font-fauna leading-6 mt-4 max-md:max-w-full">
-        Nunc sed faucibus bibendum feugiat sed interdum. Ipsum egestas
-        condimentum mi massa. In tincidunt pharetra consectetur sed duis
-        facilisis metus. Etiam egestas in nec sed et. Quis lobortis at sit
-        dictum eget nibh tortor commodo cursus.
+      Say goodbye to the days of feeling overwhelmed by legal complexities. 
       </div>
       <div className="text-black text-base font-fauna leading-6 mt-2 max-md:max-w-full">
-        Odio felis sagittis, morbi feugiat tortor vitae feugiat fusce aliquet.
-        Nam elementum urna nisi aliquet erat dolor enim. Ornare id morbi eget
-        ipsum. Aliquam senectus neque ut id eget consectetur dictum. Donec
-        posuere pharetra odio consequat scelerisque et, nunc tortor. Nulla
-        adipiscing erat a erat. Condimentum lorem posuere gravida enim posuere
-        cursus diam.
+      Embrace the power of AI-driven legal education and step into a new era where informed empowerment is not just a possibility, but a reality.  
+      
+      Welcome to a future where legal knowledge is accessible, understandable, and at your fingertips, helping you better understand the laws that govern you with confidence.
+
+
       </div>
       <img
         loading="lazy"
