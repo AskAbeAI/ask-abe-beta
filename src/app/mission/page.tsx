c7afee3f3b377cb08a53d44c14ef5e09e8fd1a8f
Madeline <datbihmad@gmail.com>
2023-11-24 19:01:41 -0500

    our mission

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -5,23 +5,30 @@ export default function MissionPage() {
   return (
     <div className="bg-[#FAF5E6] flex flex-col items-stretch">
       
-      <div className="relative flex flex-col items-stretch overflow-hidden min-h-[433px] w-full px-16 py-12 md:max-w-full md:px-5">
+      <div className="flex-col items-center overflow-hidden relative flex min-h-[382px] px-20 max-md:px-5">
         <img loading="lazy" src="/empowering.jpg" className="absolute inset-0 w-full h-full object-cover" alt="Empowering Image" />
         
-        <div className="absolute  top-1/2 text-[#FAF5E6] text-9xl font-bold font-montserrat leading-10">
+        <div className="relative text-[#FEFDF9] text-center text-5xl font-bold leading-[80px] max-w-screen-md mt-28 max-md:max-w-full max-md:mt-10">
           OUR MISSION
         </div>
         
       </div>
       <div className="items-center bg-[#FAF5E6] flex w-full flex-col px-5 py-12 max-md:max-w-full">
         <div className="flex w-full max-w-[1312px] justify-between gap-5 mt-16 mb-10 max-md:max-w-full max-md:flex-wrap max-md:mt-10">
-          <div className="text-black text-4xl font-bold leading-10 grow shrink basis-auto max-md:max-w-full">
-            Empowering
+          <div className="text-6xl text-[#2f3f3d] font-bold leading-24 grow shrink basis-auto max-md:max-w-full">
+         Empowering regular people to better understand primary source legislation.
+
           </div>
-          <div className="self-stretch text-black text-lg leading-7 grow shrink basis-auto max-md:max-w-full">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
-          </div>
+          
         </div>
+      </div>
+      <div className="relative flex flex-col items-stretch overflow-hidden min-h-[433px] w-full px-16 py-12 md:max-w-full md:px-5">
+        <img loading="lazy" src="/lib.jpg" className="absolute inset-0 w-full h-full object-cover" alt="Empowering Image" />
+        
+        <div className="absolute  top-1/2 text-[#FAF5E6] text-9xl font-bold font-montserrat leading-10">
+          
+        </div>
+        
       </div>
     </div>
   );
