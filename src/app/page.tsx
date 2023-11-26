aa59b46abb9f495dbec9c3fa0152eff926aa411a
Madeline <datbihmad@gmail.com>
2023-11-26 13:51:38 -0500

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   return (
     <div className="h-screen w-full bg-[#FAF5E6]">
       <div className="flex justify-center items-center">
-      <DisclaimerModal/>
+        <DisclaimerModal />
       </div>
 
       {/* Landing Section */}
