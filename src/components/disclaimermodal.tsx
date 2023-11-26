3b150830282f127d42c58f0b9f824f7130238b68
Madeline <datbihmad@gmail.com>
2023-11-26 11:34:25 -0500

    disclaimer

--- src/components/disclaimermodal.tsx
+++ src/components/disclaimermodal.tsx
@@ -24,7 +24,7 @@ const DisclaimerModal: React.FC = () => {
                         <div className="flex justify-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
                             <button 
                                 onClick={handleClose}
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                className="text-white font-raleway bg-[#4A4643] hover:bg-green-300 hover:text-[#4A4643] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                             >
                                 I accept
                             </button>
