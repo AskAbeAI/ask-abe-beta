1cb422a9ca816727f48565fd4de72326c90a0479
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-11-26 23:05:49 -0800

    PLEASE WORK

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -73,27 +73,27 @@ const NavBar: React.FC = () => {
             )}
           </li>
         </ul>
-        <div className="relative px-5 py-3">
+        {/* <div className="relative px-5 py-3">
           <SignedIn>
-          {/* Mount the UserButton component */}
+          
           <UserButton
             showName={true} />
         </SignedIn>
         <SignedOut>
-          {/* Signed out users get sign in button */}
+         
           <SignInButton />
         </SignedOut>
             
-        </div>
+        </div> */}
         
-        {/* <Link href="/sign-in" className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-[#FAF5E6] border border-gray-100 rounded-lg shadow-inner group">
+        <Link href="/sign-in" className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-[#FAF5E6] border border-gray-100 rounded-lg shadow-inner group">
           <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
           <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
           <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
           <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
           <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
           <span className="relative transition-colors font-monserrat duration-300 delay-200 group-hover:text-white ease">Sign In</span>
-        </Link> */}
+        </Link>
 
         {/* <button
           className="text-white text-base leading-6 whitespace-nowrap justify-center items-stretch border bg-zinc-500 rounded-full px-5 py-2 border-solid border-black"
