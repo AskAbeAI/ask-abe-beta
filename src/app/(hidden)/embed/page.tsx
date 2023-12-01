6b6d4cc47ac5fd39a0d1ccc524b5048f83349d3d
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-12-01 12:58:01 -0800

    formatting

--- src/app/(hidden)/embed/page.tsx
+++ src/app/(hidden)/embed/page.tsx
@@ -263,7 +263,7 @@ export default function EmbedPage() {
   }
 
   return (
-    <Frame title="Ask Abe Integration" style={{ width: '200vh', height: '100vh' }}
+    <Frame title="Ask Abe Integration" style={{ width: '100%', height: '100%' }}
       head={
         <>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
