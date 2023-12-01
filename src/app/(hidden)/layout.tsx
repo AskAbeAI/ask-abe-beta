d3ea98087c39edefb1ed692250b3f184f6050d05
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-11-30 23:00:27 -0800

    Sorry Madeline, I moved everything

---
+++ src/app/(hidden)/layout.tsx
@@ -0,0 +1,36 @@
+import '../globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import { ClerkProvider } from '@clerk/nextjs';
+import NavBar from '@/components/navBar';
+import PageFooter from '@/components/pageFooter';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Ask Abe',
+  description: 'AI Powered Legal Education and Research. Ask Abe is a legal research and education tool that uses AI to help you better understand the complexities of the law.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    // <ClerkProvider>
+
+      <html lang="en">
+
+        <body className={inter.className}>
+         
+          {children}
+          {/* <PageFooter /> */}
+
+        </body>
+        <Analytics />
+      </html>
+    // </ClerkProvider>
+  );
+}
