e652ab7a6382c543f19c716773155ba57bed9024
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-12-03 15:38:19 -0800

    implement followUp question integration

--- src/app/api/externalAPI/vitalia/route.ts
+++ src/app/api/externalAPI/vitalia/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { generateDirectAnswer, generateEmbedding, generateQueryRefinement, convertGroupedRowsToTextCitationPairs } from '@/lib/helpers';
+import { generateDirectAnswer, generateEmbedding, generateQueryRefinement, convertGroupedRowsToTextCitationPairs, generateFollowupQuestion } from '@/lib/helpers';
 import { GroupedRows, Jurisdiction, CitationLinks } from '@/lib/types';
 import OpenAI from "openai";
 import { insert_api_debug_log, jurisdiction_similarity_search_all_partitions, aggregateSiblingRows } from '@/lib/database';
@@ -26,7 +26,7 @@ export function OPTIONS(req: Request) {
 }
 
 export async function POST(req: Request) {
-    console.log(req.headers)
+   
     const startTime = Date.now();
     
     console.log("=== EXTERNAL VITALIA API ENDPOINT ===");
@@ -35,7 +35,7 @@ export async function POST(req: Request) {
     
     const original_question: string = requestData.question;
     const api_key: string = requestData.api_key;
-    const already_answered: string[] = requestData.already_answered;
+    let already_answered: string[] = requestData.already_answered;
 
     if (api_key !== 'ak_EjMsYGPJpLHcb48r4uCfP2ZYyrjwL') {
         return NextResponse.json({ errorMessage: `Invalid API key: ${api_key}`, status: 401 });
@@ -47,7 +47,11 @@ export async function POST(req: Request) {
 	if (supabaseKey === undefined) { throw new Error("process.env.SUPABASE_KEY is undefined!"); }
     
     try {
-        const embedding = JSON.parse(JSON.stringify(await generateEmbedding(openai, [original_question])));
+        const newQuestion = await generateFollowupQuestion(openai, original_question, already_answered);
+        if (newQuestion === original_question) {
+            already_answered = [];
+        }
+        const embedding = JSON.parse(JSON.stringify(await generateEmbedding(openai, [newQuestion])));
 
         const rows = await jurisdiction_similarity_search_all_partitions("vitalia", embedding, 0.6, 10, 15, supabaseUrl, supabaseKey);
         const jurisdiction: Jurisdiction = {id: '1', name: 'Vitalia Wiki', abbreviation: 'vitalia', corpusTitle: 'Vitalia Wiki Documentation', usesSubContentNodes: false, jurisdictionLevel: 'misc' };
@@ -55,21 +59,15 @@ export async function POST(req: Request) {
         const text_citation_pairs = convertGroupedRowsToTextCitationPairs(combined_parent_nodes);
         let instructions = `The user is looking to receive information about Vitalia 2024, which is a popup city event in the special economic zone of Prospera, on the island of Roatan Honduras. Here are some general facts that may help with answering: Location: Vitalia 2024 will be hosted in Próspera, a Special Economic Zone on the island of Roatan, Honduras.
         Duration: The pop-up city experience will take place from Jan 6th to March 1st 2024, and encourages a minimum stay of 1 month, with a focus on participants willing to spend at least 2 months.
-        Cost: Room pricing ranges from $1,000 to $3,000 per month, including accommodation and shared amenities like a gym and shared cars.
-        Who's Coming: The resident profile consists of scientists, entrepreneurs, artists, and thinkers specializing in fields like longevity biotechnology, healthcare, and decentralized governance.
-        Work Compatibility: Vitalia is not a conference; participants are encouraged to bring their work with them.
-        Amenities: The package includes medium-range private suites, free-use facilities like a gym and pool, on-site healthcare, and logistical services like car pooling.
-        Additional Services: Childcare services and a variety of wellness activities organized by residents are available.
-        Local Community: Roatan has a diverse and friendly local community with many accepting Bitcoin and other cryptocurrencies.
-        Acceleration of Longevity Innovation: Vitalia, long-term, aims to eliminate bureaucratic roadblocks to speed up clinical trials and lower costs in the longevity field.
+        
         
         Answer the user's more specific question as best you can. For broad or general questions, it's okay to give a general overview.
         `
         if (already_answered.length > 0) {
-            instructions += `The following questions have already been answered: ${already_answered.join(', ')}. Please do not repeat the same information.`;
+            instructions += `The following questions have already been answered: ${already_answered.join(', ')}. If there are previous questions, make sure to integrate them into your answer as their current question is most likely a followup question.`;
         }
         
-        const direct_answer = await generateDirectAnswer(openai, original_question, instructions,  text_citation_pairs);
+        const direct_answer = await generateDirectAnswer(openai, newQuestion, instructions,  text_citation_pairs);
         const citationLinks: CitationLinks = {};
         for (const row of rows) {
             citationLinks[row.citation] = row.link!
