0254d25615a3939dd6d983dc1cf7e21362887f37
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-12-03 16:54:04 -0800

    better citationLinks

--- src/app/(hidden)/embed/page.tsx
+++ src/app/(hidden)/embed/page.tsx
@@ -206,31 +206,37 @@ export default function EmbedPage() {
     
     console.log("Handling clarification answer!");
     if(response.response === "No, Reach Out to An Organizer") {
+      const citationLinks: CitationLinks = {}
+      citationLinks["Organizer"] = "https://t.me/tailsph"
       const params: ContentBlockParams = {
         type: ContentType.AnswerVitalia,
         content: "I'm sorry I couldn't help you find what you're looking for. I have provided the contact information for one of the organizers of Vitalia below. Please reach out to them for further assistance. ### Organizer ###",
         fake_stream: true,
         concurrentStreaming: false,
-        citationLinks: {citation: "Organizer", link: "https://t.me/tailsph"} as CitationLinks
+        citationLinks: citationLinks
       };
       await addContentBlock(createNewBlock(params));
       
     } else if (response.response === "Yes") {
+      const citationLinks: CitationLinks = {}
+      citationLinks["Vitalia Wiki"] = "https://wiki.vitalia.city/"
       const params: ContentBlockParams = {
           type: ContentType.AnswerVitalia,
           content: "Great! Glad I could help! Reach out if you have any other questions or check out our ### Vitalia Wiki ###",
           fake_stream: true,
           concurrentStreaming: false,
-          citationLinks: {citation: "Vitalia Wiki", link: "https://wiki.vitalia.city/"} as CitationLinks
+          citationLinks: citationLinks
       };
       await addContentBlock(createNewBlock(params));
     } else if (response.response === "No"){
+      const citationLinks: CitationLinks = {}
+      citationLinks["Vitalia Wiki"] = "https://wiki.vitalia.city/"
       const params: ContentBlockParams = {
         type: ContentType.AnswerVitalia,
         content: "I'm sorry I couldn't help you find what you're looking for. The ### Vitalia Wiki ### has a lot of information that might be helpful.",
         fake_stream: true,
         concurrentStreaming: false,
-        citationLinks: {citation: "Vitalia Wiki", link: "https://wiki.vitalia.city/"} as CitationLinks
+        citationLinks: citationLinks
       };
       await addContentBlock(createNewBlock(params));
     }
