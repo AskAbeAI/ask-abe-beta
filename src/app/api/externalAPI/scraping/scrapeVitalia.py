4be2e79e5cabf6a325e584b60e2f81c325d3d842
Will Diamond <59883254+spartypkp@users.noreply.github.com>
2023-12-16 12:38:40 -0800

    better wqy

--- src/app/api/externalAPI/scraping/scrapeVitalia.py
+++ src/app/api/externalAPI/scraping/scrapeVitalia.py
@@ -17,26 +17,16 @@ client = OpenAI(
     api_key="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",
 )
 
+ALL_URLS = []
+ALL_TITLES = []
 def main():
     print("Hello!")
     exit(1)
     read_urls_to_list()
-    read_urls_to_file()
     
-    
-    file = open(f"{DIR}/urls.txt", "r")
-    url = ""
-    title = ""
-    for i, line in enumerate(file):
-        print(line)
-        if i % 2 == 0:
-            url = line
-        else:
-            title = line
-            cleaned_title = title.strip()
-            print(cleaned_title)
-
-            scrape_title(url, cleaned_title)
+    for i, url in enumerate(ALL_URLS):
+        title = ALL_TITLES[i]
+        scrape_title(url, title)
         
     
 def read_urls_to_list():
@@ -45,194 +35,168 @@ def read_urls_to_list():
     text = data.decode('utf-8') # a `str`; 
     soup = BeautifulSoup(text)
     all_urls = soup.find_all("a")
-    with open(f"{DIR}/urls.txt","w") as write_file:
-        for url in all_urls:
-            print(url.get_text())
-            link = url['href']
-            print(link)
-            if ("https" not in link):
-                write_file.write(BASE_URL + link + "\n")
-                cleaned_title = url.get_text().strip()  
-                cleaned_title = cleaned_title.replace(" ", "_")
-                cleaned_title = cleaned_title.replace("__", "_")
-                cleaned_title = cleaned_title.replace("“", "")
-                cleaned_title = cleaned_title.replace("”", "")
-                write_file.write(cleaned_title + "\n")
+    for url in all_urls:
+        print(url.get_text())
+        link = url['href']
+        print(link)
+        if ("https" not in link):
+            ALL_URLS.append(BASE_URL + link + "\n")
+            cleaned_title = url.get_text().strip()  
+            cleaned_title = cleaned_title.replace(" ", "_")
+            cleaned_title = cleaned_title.replace("__", "_")
+            cleaned_title = cleaned_title.replace("“", "")
+            cleaned_title = cleaned_title.replace("”", "")
+            ALL_TITLES.append(cleaned_title + "\n")
     
-    write_file.close()
-
-def read_urls_to_file():
-    file = open(f"{DIR}/urls.txt", "r")
-
-    url = ""
-    title = ""
-    for i, line in enumerate(file):
-        if i % 2 == 0:
-            url = line
-        else:
-            title = line
-            print("Url: ", url)
-            print("Title: ", title)
-            
-            read_top_level_title_to_file(url, title)
 
 
-def read_top_level_title_to_file(url, top_level_title):
-    response = urllib.request.urlopen(url)
-    data = response.read()      # a `bytes` object
-    text = data.decode('utf-8') # a `str`; 
-    soup = BeautifulSoup(text)
-    cleaned_title = top_level_title.strip()
-    cleaned_title = cleaned_title.replace(" ", "_")
-    pretty = soup.prettify()
-    with open(f"{DIR}/{cleaned_title}.txt","w") as write_file:
-        write_file.write(pretty)
-    write_file.close()
 
 def scrape_title(url, top_level_title):
     print("in scrape title!")
-    with open(f"{DIR}/{top_level_title}.txt","r") as read_file:
-        raw_soup = BeautifulSoup(read_file, 'html.parser')
-        
-        soup = raw_soup.find('article', {'class': 'notion-root'})
-        if soup is None:
-            print("Soup was none!")
-            soup = raw_soup.find('article', {'class': 'notion-root full-width'})
+    response = urllib.request.urlopen(url)
+    data = response.read()      # a `bytes` object
+    text = data.decode('utf-8') # a `str`; 
+    raw_soup = BeautifulSoup(text)
+    
+    soup = raw_soup.find('article', {'class': 'notion-root'})
+    if soup is None:
+        print("Soup was none!")
+        soup = raw_soup.find('article', {'class': 'notion-root full-width'})
 
-        
-        
-        node_id = None
-        node_top_level_title = top_level_title
-        node_type = "content"
-        node_level_classifier="SECTION"
-        node_text = []
-        node_text_embedding = []
-        node_citation = f"{top_level_title}/Introduction"
-        node_link = ""
-        node_addendum = ""
-        node_name = "Introduction"
-        node_name_embedding = None
-        node_summary = None
-        node_summary_embedding = None
-        node_hyde = None
-        node_hyde_embedding = None
-        node_parent = None
-        node_direct_children = None
-        node_siblings = None
-        node_references = None
-        node_incoming_references = None
-        node_tags = None
-        
-        specific_link = ""
-        # Iterate over all html elements until you find a new <h1> tag
-        for tag in soup.find_all(recursive=False):
-            # Add all text content of non h1 tags to the section
-            if tag.name == "h1" or tag.name == "h2" or tag.name == "h3":
-                node_id = f"vitalia/wiki/{top_level_title}/{node_name}"
-                if (specific_link ==""):
-                    node_link = url.strip()
-                else:
-                    node_link = url.strip() + "#" + specific_link.strip()
-                aggregated_text = "".join(node_text)
-                
-                node_text_embedding = create_embedding(aggregated_text)
-                row_data = {"node_id": node_id, 
-                    "node_top_level_title": top_level_title,
-                    "node_type": node_type,
-                    "node_level_classifier": node_level_classifier,
-                    "node_text": node_text,
-                    "node_text_embedding": node_text_embedding,
-                    "node_citation": node_citation,
-                    "node_link": node_link,
-                    "node_addendum": node_addendum,
-                    "node_name": node_name,
-                    "node_name_embedding": node_name_embedding,
-                    "node_summary": node_summary,
-                    "node_summary_embedding": node_summary_embedding,
-                    "node_hyde": node_hyde,
-                    "node_hyde_embedding": node_hyde_embedding,
-                    "node_parent": node_parent,
-                    "node_direct_children": node_direct_children,
-                    "node_siblings": node_siblings,
-                    "node_references": node_references,
-                    "node_incoming_references": node_incoming_references,
-                    "node_tags": node_tags}
-                for i in range(2, 10):
-                    try:
-                        response = insert_row_to_supabase("vitalia_node", row_data)
-                        break
-                    except:
-                        # Remove all characters from row_data.node_id after "-version"
-                        temp = row_data["node_id"]
-                        if("-version" in row_data["node_id"]):
-                            temp = row_data["node_id"].split("-version")[0]
-
-                        row_data["node_id"] = temp + f"-version_{i}"
-                        
-                specific_link = tag['id']
-                node_name = tag.get_text().strip()
-                node_text = []
-                node_text_embedding = []
-                node_citation = f"{top_level_title}/{node_name}"
-                node_addendum = ""
-                node_name_embedding = None
-                node_summary = None
-                node_summary_embedding = None
-                node_hyde = None
-                node_hyde_embedding = None
-                node_parent = None
-                node_direct_children = None
-                node_siblings = None
-                node_references = None
-                node_incoming_references = None
-                node_tags = None
+    
+    
+    node_id = None
+    node_top_level_title = top_level_title
+    node_type = "content"
+    node_level_classifier="SECTION"
+    node_text = []
+    node_text_embedding = []
+    node_citation = f"{top_level_title}/Introduction"
+    node_link = ""
+    node_addendum = ""
+    node_name = "Introduction"
+    node_name_embedding = None
+    node_summary = None
+    node_summary_embedding = None
+    node_hyde = None
+    node_hyde_embedding = None
+    node_parent = None
+    node_direct_children = None
+    node_siblings = None
+    node_references = None
+    node_incoming_references = None
+    node_tags = None
+    
+    specific_link = ""
+    # Iterate over all html elements until you find a new <h1> tag
+    for tag in soup.find_all(recursive=False):
+        # Add all text content of non h1 tags to the section
+        if tag.name == "h1" or tag.name == "h2" or tag.name == "h3":
+            node_id = f"vitalia/wiki/{top_level_title}/{node_name}"
+            if (specific_link ==""):
+                node_link = url.strip()
             else:
-                text = tag.get_text().strip()
-                cleaned_text = re.sub(r'\s+', ' ', text).strip()
-                node_text.append(cleaned_text)
-                
+                node_link = url.strip() + "#" + specific_link.strip()
+            aggregated_text = "".join(node_text)
+            
+            node_text_embedding = create_embedding(aggregated_text)
+            row_data = {"node_id": node_id, 
+                "node_top_level_title": top_level_title,
+                "node_type": node_type,
+                "node_level_classifier": node_level_classifier,
+                "node_text": node_text,
+                "node_text_embedding": node_text_embedding,
+                "node_citation": node_citation,
+                "node_link": node_link,
+                "node_addendum": node_addendum,
+                "node_name": node_name,
+                "node_name_embedding": node_name_embedding,
+                "node_summary": node_summary,
+                "node_summary_embedding": node_summary_embedding,
+                "node_hyde": node_hyde,
+                "node_hyde_embedding": node_hyde_embedding,
+                "node_parent": node_parent,
+                "node_direct_children": node_direct_children,
+                "node_siblings": node_siblings,
+                "node_references": node_references,
+                "node_incoming_references": node_incoming_references,
+                "node_tags": node_tags}
+            for i in range(2, 10):
+                try:
+                    response = insert_row_to_supabase("vitalia_node", row_data)
+                    break
+                except:
+                    # Remove all characters from row_data.node_id after "-version"
+                    temp = row_data["node_id"]
+                    if("-version" in row_data["node_id"]):
+                        temp = row_data["node_id"].split("-version")[0]
 
-        node_id = f"vitalia/wiki/{top_level_title}/{node_name}"
-        if (specific_link ==""):
-            node_link = url.strip()
+                    row_data["node_id"] = temp + f"-version_{i}"
+                    
+            specific_link = tag['id']
+            node_name = tag.get_text().strip()
+            node_text = []
+            node_text_embedding = []
+            node_citation = f"{top_level_title}/{node_name}"
+            node_addendum = ""
+            node_name_embedding = None
+            node_summary = None
+            node_summary_embedding = None
+            node_hyde = None
+            node_hyde_embedding = None
+            node_parent = None
+            node_direct_children = None
+            node_siblings = None
+            node_references = None
+            node_incoming_references = None
+            node_tags = None
         else:
-            node_link = url.strip() + "#" + specific_link.strip()
-        
-        aggregated_text = "".join(node_text)
-        
-        node_text_embedding = create_embedding(aggregated_text)
-        row_data = {"node_id": node_id, 
-                    "node_top_level_title": top_level_title,
-                    "node_type": node_type,
-                    "node_level_classifier": node_level_classifier,
-                    "node_text": node_text,
-                    "node_text_embedding": node_text_embedding,
-                    "node_citation": node_citation,
-                    "node_link": node_link,
-                    "node_addendum": node_addendum,
-                    "node_name": node_name,
-                    "node_name_embedding": node_name_embedding,
-                    "node_summary": node_summary,
-                    "node_summary_embedding": node_summary_embedding,
-                    "node_hyde": node_hyde,
-                    "node_hyde_embedding": node_hyde_embedding,
-                    "node_parent": node_parent,
-                    "node_direct_children": node_direct_children,
-                    "node_siblings": node_siblings,
-                    "node_references": node_references,
-                    "node_incoming_references": node_incoming_references,
-                    "node_tags": node_tags}
-        for i in range(2, 10):
-            try:
-                response = insert_row_to_supabase("vitalia_node", row_data)
-                break
-            except:
-                # Remove all characters from row_data.node_id after "-version"
-                temp = row_data["node_id"]
-                if("-version" in row_data["node_id"]):
-                    temp = row_data["node_id"].split("-version")[0]
+            text = tag.get_text().strip()
+            cleaned_text = re.sub(r'\s+', ' ', text).strip()
+            node_text.append(cleaned_text)
+            
 
-                row_data["node_id"] = temp + f"-version_{i}"
+    node_id = f"vitalia/wiki/{top_level_title}/{node_name}"
+    if (specific_link ==""):
+        node_link = url.strip()
+    else:
+        node_link = url.strip() + "#" + specific_link.strip()
+    
+    aggregated_text = "".join(node_text)
+    
+    node_text_embedding = create_embedding(aggregated_text)
+    row_data = {"node_id": node_id, 
+                "node_top_level_title": top_level_title,
+                "node_type": node_type,
+                "node_level_classifier": node_level_classifier,
+                "node_text": node_text,
+                "node_text_embedding": node_text_embedding,
+                "node_citation": node_citation,
+                "node_link": node_link,
+                "node_addendum": node_addendum,
+                "node_name": node_name,
+                "node_name_embedding": node_name_embedding,
+                "node_summary": node_summary,
+                "node_summary_embedding": node_summary_embedding,
+                "node_hyde": node_hyde,
+                "node_hyde_embedding": node_hyde_embedding,
+                "node_parent": node_parent,
+                "node_direct_children": node_direct_children,
+                "node_siblings": node_siblings,
+                "node_references": node_references,
+                "node_incoming_references": node_incoming_references,
+                "node_tags": node_tags}
+    for i in range(2, 10):
+        try:
+            response = insert_row_to_supabase("vitalia_node", row_data)
+            break
+        except:
+            # Remove all characters from row_data.node_id after "-version"
+            temp = row_data["node_id"]
+            if("-version" in row_data["node_id"]):
+                temp = row_data["node_id"].split("-version")[0]
+
+            row_data["node_id"] = temp + f"-version_{i}"
         
 
 
