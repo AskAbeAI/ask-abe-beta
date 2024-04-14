49c12d5b6555acb0800f0a95e6af750a48f1c476
spartypkp <will@diamondquarters.com>
2024-04-14 16:59:21 -0700

    god damn CORS

--- src/app/api/externalAPI/privateQA/route.ts
+++ src/app/api/externalAPI/privateQA/route.ts
@@ -11,19 +11,19 @@ const openai = new OpenAI({
   apiKey: openAiKey,
 });
 
-// export function OPTIONS(req: Request) {
+export function OPTIONS(req: Request) {
     
-//     // Set CORS headers
-//     const headers = {
-//         'Access-Control-Allow-Origin': 'https://web.postman.co', // Modify as needed for your use case
-//         'Access-Control-Allow-Methods': 'POST, OPTIONS',
-//         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-//         // Include any other headers you might need for your requests
-//     };
+    // Set CORS headers
+    const headers = {
+        'Access-Control-Allow-Origin': 'https://concierge-live.vercel.app/', // Modify as needed for your use case
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        // Include any other headers you might need for your requests
+    };
 
-//     // Return the response with CORS headers and no body
-//     return new NextResponse(null, { status: 204, headers });
-// }
+    // Return the response with CORS headers and no body
+    return new NextResponse(null, { status: 204, headers });
+}
 interface PrivateQARequest {
     abe_api_key: string;
     question: string;
