bb12374720700ee7db3cace5d30ae67d0aef601c
spartypkp <will@diamondquarters.com>
2024-06-02 10:58:57 -0700

    adding threejs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {transpilePackages: ['three'],}
 
+const withTM = require('next-transpile-modules')(['three'])
+module.exports = withTM()
 
-module.exports = nextConfig;
---
+++ src/lib/threejs/three.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { r3f } from './tunellRat'
+
+export const Three = ({
+	children,
+  }: {
+	children: React.ReactNode;
+  }) => {
+  return <r3f.In>{children}</r3f.In>
+}
